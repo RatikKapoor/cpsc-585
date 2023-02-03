3981488f8824dce201e903d8c13529595fae3826
ybayoumy <yassin.omgb@yahoo.com>
2023-02-02 23:31:37 -0700

    Revert "initial system"
    This reverts commit f0201d6f67efbda489b9dcc044f9546868c8b87a.

--- DustRiders/DustRiders/Entity.h
+++ DustRiders/DustRiders/Entity.h
@@ -2,17 +2,12 @@
 
 #include <string>
 #include "Model.h"
-#include "ShaderProgram.h"
-#include "Texture.h"
 #include "Transform.h"
 
 class Entity
 {
 public:
 	std::string name;
-	Transform* transform;
-
 	Model* model;
-	ShaderProgram* shaderProgram;
-	Texture* texture;
+	Transform* transform;
 };
--- DustRiders/DustRiders/Model.h
+++ DustRiders/DustRiders/Model.h
@@ -3,35 +3,15 @@
 #include <glm/glm.hpp>
 #include <vector>
 
-#include "Geometry.h"
+struct Vertex
+{
+	glm::vec3 position;
+	glm::vec3 color;
+};
 
 class Model
 {
 public:
-	// These vectors are parallel to represent a vertex overall
-	std::vector<glm::vec3> positions;
-	std::vector<glm::vec3> colors;
-	//std::vector<glm::vec3> normals;
-	//std::vector<glm::vec2> texCoords;
-
-	GPU_Geometry geometry;
-
-	int use() {
-		geometry.bind();
-
-		return positions.size();
-	}
-
-	Model(std::vector<glm::vec3>& verts, std::vector<glm::vec3>& cols) 
-	: positions(verts)
-	, colors(cols)
-	//, normals(norms)
-	//, texCoords(tex)
-	{
-		geometry.bind();
-		geometry.setVerts(positions);
-		geometry.setCols(colors);
-		//geometry.setNormals(normals);
-		//geometry.setTexCoords(texCoords);
-	}
+	std::vector<Vertex> verticies;
+	glm::mat4 modelMatrix;
 };
