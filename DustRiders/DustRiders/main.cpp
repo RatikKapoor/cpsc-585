77f530769e24479c15956bd34e150884ffd258e2
zheng-628 <zheng.chen1@ucalgary.ca>
2023-02-17 11:41:12 -0700

    Update main.cpp

--- DustRiders/DustRiders/main.cpp
+++ DustRiders/DustRiders/main.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <cmath>
 #include <string>
+#include <memory>
 
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
@@ -19,18 +20,18 @@
 #include "Overlay.h"
 #include "ShaderProgram.h"
 #include "Geometry.h"
-#include "SoundDevice.h"
-#include "SoundBuffer.h"
-#include "SoundSource.h"
-#include "MusicBuffer.h"
 #include "RenderingSystem.h"
 #include "InputHandler.h"
 #include "Camera.h"
 #include "Vehicle.h"
+#include "SoundDevice.h"
+#include "SoundBuffer.h"
+#include "SoundSource.h"
+#include "MusicBuffer.h"
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 }
@@ -104,7 +105,7 @@ float beepGas(int jsID)
 	if (glfwJoystickPresent(jsID))
 	{
 		int count;
-		const float *axis = glfwGetJoystickAxes(jsID, &count);
+		const float* axis = glfwGetJoystickAxes(jsID, &count);
 
 		if (axis[XBOX_L_YAXIS] < -0.1 && !hasStarted)
 		{
@@ -125,7 +126,7 @@ float beepSteer(int jsID)
 	if (glfwJoystickPresent(jsID))
 	{
 		int count;
-		const float *axis = glfwGetJoystickAxes(jsID, &count);
+		const float* axis = glfwGetJoystickAxes(jsID, &count);
 		return axis[XBOX_R_XAXIS];
 	}
 	else
@@ -140,9 +141,9 @@ CarAction isBeepBeep(int jsID)
 	{
 		int count;
 
-		const unsigned char *buttons = glfwGetJoystickButtons(jsID, &count);
+		const unsigned char* buttons = glfwGetJoystickButtons(jsID, &count);
 
-		const float *axis = glfwGetJoystickAxes(jsID, &count);
+		const float* axis = glfwGetJoystickAxes(jsID, &count);
 
 		if (buttons[XBOX_RB])
 		{
@@ -177,81 +178,34 @@ int main()
 	Window window(800, 800, "DustRiders");
 	window.setCallbacks(std::make_shared<DustRidersWindowCallbacks>());
 
+	// Disable at your own risk, will not start the game until there is a valid controller connected
+	if (glfwJoystickPresent(GLFW_JOYSTICK_1))
+	{
+		JoystickHandler::addJS(GLFW_JOYSTICK_1);
+	}
+	else
+	{
+		while (!glfwJoystickPresent(GLFW_JOYSTICK_1))
+		{
+			std::cout << "PLEASE CONNECT A CONTROLLER" << std::endl;
+			glfwPollEvents();
+		}
+	}
+
 	PhysicsSystem physics;
 	RenderingSystem renderer;
 	Overlay overlay;
-	ShaderProgram *basicShader = renderer.compileShader("basic", "basic.vert", "basic.frag");
+	ShaderProgram* carShader = renderer.compileShader("car", "./car.vert", "./car.frag");
+	ShaderProgram* basicShader = renderer.compileShader("basic", "./basic.vert", "./basic.frag");
 
-#pragma region Verts
-	std::vector<Vertex> cubeVerts{
-		Vertex{glm::vec3{0.5f, 0.5f, 0.5f}, glm::vec3{0.0f, 1.0f, 1.0f}, glm::vec2(0.0f)},
-		Vertex{glm::vec3{-0.5f, 0.5f, 0.5f}, glm::vec3{0.0f, 1.0f, 1.0f}, glm::vec2(0.0f)},
-		Vertex{glm::vec3{-0.5f, -0.5f, 0.5f}, glm::vec3{0.0f, 0.0f, 1.0f}, glm::vec2(0.0f)},
-		Vertex{glm::vec3{0.5f, -0.5f, 0.5f}, glm::vec3{0.0f, 0.0f, 1.0f}, glm::vec2(0.0f)},
-		Vertex{glm::vec3{0.5f, 0.5f, -0.5f}, glm::vec3{1.0f, 0.0f, 1.0f}, glm::vec2(0.0f)},
-		Vertex{glm::vec3{-0.5f, 0.5f, -0.5f}, glm::vec3{1.0f, 0.0f, 1.0f}, glm::vec2(0.0f)},
-		Vertex{glm::vec3{-0.5f, -0.5f, -0.5f}, glm::vec3{1.0f, 0.0f, 0.0f}, glm::vec2(0.0f)},
-		Vertex{glm::vec3{0.5f, -0.5f, -0.5f}, glm::vec3{1.0f, 0.0f, 0.0f}, glm::vec2(0.0f)}};
-	std::vector<unsigned int> cubeIndices{
-		// front face
-		0, 1, 2,
-		0, 2, 3,
 
-		// right face
-		4, 0, 3,
-		4, 3, 7,
+	// To load in a model, just use "loadModelFromFile". Textures are handled automatically. 
+	Model* testCarModel = renderer.loadModelFromFile("TestCar", "./assets/models/better-car-v2.obj");
+	Model* testRock = renderer.loadModelFromFile("TestRock", "./assets/models/test-obstacle-rock.obj");
+	Model* groundPlane = renderer.loadModelFromFile("GroundPlane", "./assets/models/ground-plane.obj");
 
-		// back face
-		5, 4, 7,
-		5, 7, 6,
 
-		// left face
-		1, 5, 6,
-		1, 6, 2,
-
-		// top face
-		0, 4, 5,
-		0, 5, 1,
-
-		// bottom face
-		2, 3, 7,
-		2, 7, 6};
-	Model *cubeModel = new Model();
-	cubeModel->meshes.push_back(Mesh(cubeVerts, cubeIndices));
-	cubeModel = renderer.addModel("cube", cubeModel);
-#pragma endregion
-
-	ShaderProgram *basicShader = renderer.compileShader("basic", "../DustRiders/basic.vert", "../DustRiders/basic.frag");
-
-	entityList.reserve(465);
-	for (int i = 0; i < 465; i++)
-	{
-		entityList.emplace_back(new Entity());
-		entityList.back()->transform = physics.transformList[i];
-
-		entityList.back()->model = cubeModel;
-		entityList.back()->shaderProgram = basicShader;
-	}
-
-	// camera.setFocusEntity(entityList[0]);
-
-	// glfwSetFramebufferSizeCallback(window., framebuffer_size_callback);
-	SoundDevice *mysounddevice = SoundDevice::get();
-	uint32_t /*ALuint*/ sound1 = SoundBuffer::get()->addSoundEffect("../sound/blessing.ogg");
-
-	SoundSource mySpeaker;
-	alSourcePlay(mySpeaker.Play(sound1));
-
-	MusicBuffer myMusic("../sound/TownTheme.wav");
-	std::cout << "playing town theme music...\n";
-	myMusic.Play();
-
-	ALint state = AL_PLAYING;
-	std::cout << "playing sound\n";
-
-	std::vector<Entity *> entityList;
-
-	JoystickHandler::addJS(GLFW_JOYSTICK_1);
+	std::vector<Entity*> entityList;
 
 	Vehicle v1(physics);
 	{
@@ -259,19 +213,28 @@ int main()
 		v1.initVehicle(PxVec3(0.f, 0.5f, 0.f));
 		entityList.emplace_back(new Entity());
 		entityList.back()->transform = physics.transformList.back();
-		entityList.back()->model = cubeModel;
+		entityList.back()->model = testCarModel;
 		entityList.back()->shaderProgram = basicShader;
-		entityList.back()->scale = glm::vec3{1.0f, 1.0f, 2.0f};
+		entityList.back()->scale = glm::vec3{ 1.f, 1.f, 1.f };
 	}
+
+	// Adds ground plane
+	entityList.emplace_back(new Entity());
+	entityList.back()->transform = new Transform();
+	entityList.back()->transform->position = glm::vec3(0.f, -0.5f, 0.f);
+	entityList.back()->model = groundPlane;
+	entityList.back()->shaderProgram = basicShader;
+	entityList.back()->scale = glm::vec3{ 1.f, 1.f, 1.f };
+
 	Vehicle v2(physics);
 	{
 		// Setup v2
 		v2.initVehicle(PxVec3(-2.f, 0.5f, 0.f));
 		entityList.emplace_back(new Entity());
 		entityList.back()->transform = physics.transformList.back();
-		entityList.back()->model = cubeModel;
+		entityList.back()->model = testCarModel;
 		entityList.back()->shaderProgram = basicShader;
-		entityList.back()->scale = glm::vec3{1.0f, 1.0f, 2.0f};
+		entityList.back()->scale = glm::vec3{ 1.f, 1.f, 1.f };
 	}
 	Vehicle v3(physics);
 	{
@@ -279,13 +242,13 @@ int main()
 		v3.initVehicle(PxVec3(2.f, 0.5f, 0.f));
 		entityList.emplace_back(new Entity());
 		entityList.back()->transform = physics.transformList.back();
-		entityList.back()->model = cubeModel;
+		entityList.back()->model = testCarModel;
 		entityList.back()->shaderProgram = basicShader;
-		entityList.back()->scale = glm::vec3{1.0f, 1.0f, 2.0f};
+		entityList.back()->scale = glm::vec3{ 1.f, 1.f, 1.f };
 	}
 
 	// Follow the Player Vehicle
-	Camera camera(entityList[0], glm::radians(45.0f), 20.0);
+	Camera camera(entityList[0], glm::radians(15.0f), 10.0);
 
 	int obstacleCount = 0;
 	for (float dist = 0; dist <= 1000.5f; dist += 10.0f)
@@ -295,33 +258,46 @@ int main()
 
 		if (obstacleCount % 2 == 0)
 		{
-			entityList.back()->transform->position = glm::vec3{-7.0f, 0.0f, dist};
+			entityList.back()->transform->position = glm::vec3{ -7.0f, 0.0f, dist };
 		}
 		else
 		{
-			entityList.back()->transform->position = glm::vec3{7.0f, 0.0f, dist};
+			entityList.back()->transform->position = glm::vec3{ 7.0f, 0.0f, dist };
 		}
 
-		entityList.back()->model = cubeModel;
+		entityList.back()->model = testRock;
 		entityList.back()->shaderProgram = basicShader;
-		entityList.back()->scale = glm::vec3{3.0f, 1.0f, 0.5f};
+		entityList.back()->scale = glm::vec3{ 2.0f, 2.0f, 2.0f };
 
 		obstacleCount++;
 	}
+
+	SoundDevice* mysounddevice = SoundDevice::get();
+	uint32_t /*ALuint*/ sound1 = SoundBuffer::get()->addSoundEffect("../sound/blessing.ogg");
+
+	SoundSource mySpeaker;
+	alSourcePlay(mySpeaker.Play(sound1));
+
+	MusicBuffer myMusic("../sound/TownTheme.wav");
+	std::cout << "playing town theme music...\n";
+	myMusic.Play();
+
+
+	ALint state = AL_PLAYING;
+	std::cout << "playing sound\n";
+
 
 	double lastTime = 0.0f;
 	int i = 0;
 	while (!window.shouldClose())
 	{
-
-		// Game Section
-		// processInput(window.get);
 		if (state == AL_PLAYING && alGetError() == AL_NO_ERROR)
 		{
 			myMusic.UpdateBufferStream();
 
 			alGetSourcei(myMusic.getSource(), AL_SOURCE_STATE, &state);
 		}
+
 
 		auto t = glfwGetTime();
 		if (t - lastTime > 0.0167)
@@ -355,8 +331,6 @@ int main()
 				v2.stepPhysics(deltaT, -accel, 0);
 				accel = (double)std::rand() / RAND_MAX * 0.5 + 0.2;
 				v3.stepPhysics(deltaT, -accel, 0);
-
-				// Win condition
 				if (physics.transformList[0]->position.z - physics.transformList[1]->position.z > 50.f)
 				{
 					// Game won
