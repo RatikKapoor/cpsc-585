01b77b101a87ffdf11e57c36f66da67010ad0ce0
brooke-k <brooke.kindleman@gmail.com>
2023-03-05 22:05:25 -0700

    feat: Raygun beam is now a physx object
    - Currently collides with other objects and moves in unpredictable
      directions

---
+++ DustRiders/DustRiders/RayBeam.cpp
@@ -0,0 +1,31 @@
+#include "RayBeam.h"
+
+RayBeam::RayBeam(std::string n,
+                 Model *m,
+                 ShaderProgram *sp,
+                 glm::vec3 s,
+                 PhysicsProvider *pp,
+                 PxVec3 pos = {0.f, 0.f, 0.f},
+                 unsigned int mat = 0) : PhysicsEntity(n, m, sp, s, pp, pos, mat)
+{
+  isActive = false;
+  shouldRender = false;
+  initBeam(pos);
+}
+
+void RayBeam::initBeam(PxVec3 pos)
+{
+  auto shape = this->gPhysics->createShape(PxBoxGeometry(1.5, 5.0, 1.5), *gMaterial);
+  shape->setFlag(PxShapeFlag::eSIMULATION_SHAPE, true);
+
+  PxTransform localTm(pos);
+  PxRigidDynamic *body = gPhysics->createRigidDynamic(localTm);
+  body->setGlobalPose(localTm);
+  body->setRigidBodyFlag(PxRigidBodyFlag::eKINEMATIC, true);
+  body->attachShape(*shape);
+  PxRigidBodyExt::updateMassAndInertia(*body, 10.f);
+  gScene->addActor(*body);
+  physicsProvider->AddEntity(body, this->transform);
+
+  shape->release();
+}
