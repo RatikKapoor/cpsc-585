ce77db93e880d5a1aa47c1b44f104403b89b2afb
Ratik Kapoor <ratikrk@gmail.com>
2023-03-25 17:04:44 -0600

    feat: Setup chunking initialization

--- DustRiders/DustRiders/ChunkHandler.cpp
+++ DustRiders/DustRiders/ChunkHandler.cpp
@@ -1,1 +1,13 @@
 #include "ChunkHandler.h"
+
+EntityComponentSystem* ChunkHandler::ecs;
+PhysicsProvider* ChunkHandler::physics;
+
+void ChunkHandler::initialize(EntityComponentSystem& system, PhysicsProvider* provider) {
+	ecs = &system;
+	physics = provider;
+
+	(*ecs)["ground"] = new Ground("ground", ModelProvider::straightPath, ShaderProvider::carShader, glm::vec3(1.f), physics, PxVec3(0.f, 0.f, 0.f), 0);
+	(*ecs)["ground2"] = new Ground("ground2", ModelProvider::dividedPath, ShaderProvider::carShader, glm::vec3(1.f), physics, PxVec3(0.f, 0.f, 300.f), 0);
+	(*ecs)["ground3"] = new Ground("ground3", ModelProvider::straightPath, ShaderProvider::carShader, glm::vec3(1.f), physics, PxVec3(0.f, 0.f, 600.f), 0);
+}
