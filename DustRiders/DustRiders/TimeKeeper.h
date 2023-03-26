ab0ebc7974e52ad5ca6cbdebb8ea81c5043eeef3
brooke-k <brooke.kindleman@gmail.com>
2023-03-26 15:19:33 -0600

    feat: Added cooldown period for firing
    - Currently set at 2 seconds but can be changed

--- DustRiders/DustRiders/TimeKeeper.h
+++ DustRiders/DustRiders/TimeKeeper.h
@@ -68,9 +68,3 @@ private:
   static double lastTime;
   static double deltaT;
 };
-
-bool TimeKeeper::initialized = false;
-bool   TimeKeeper::paused = false;
-double TimeKeeper::currentTime = 0.0;
-double TimeKeeper::lastTime = 0.0;
-double TimeKeeper::deltaT = 0.0;
