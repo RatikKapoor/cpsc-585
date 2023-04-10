6679c0930ad665d21d70226b5dede107aa756be5
brooke-k <brooke.kindleman@gmail.com>
2023-04-10 00:12:17 -0600

    feat: All models added to game

--- DustRiders/DustRiders/ModelProvider.h
+++ DustRiders/DustRiders/ModelProvider.h
@@ -4,18 +4,22 @@
 #include "Model.h"
 #include "RenderingSystem.h"
 
-class ModelProvider : public Provider {
+class ModelProvider : public Provider
+{
 public:
-	static void initialize(RenderingSystem&);
+	static void initialize(RenderingSystem &);
 
-	static Model* carModel;
-	static Model* rock1;
-	static Model* rock2;
-	static Model* rock3;
-	static Model* rayBeam;
-	static Model* straightPath;
-	static Model* dividedPath;
-	static Model* speedupBlock;
-	static Model* slowdownBlock;
-
+	static Model *carModel;
+	static Model *carFlames;
+	static Model *rock1;
+	static Model *rock2;
+	static Model *rock3;
+	static Model *rayBeam;
+	static Model *straightPath;
+	static Model *oneDividedPath;
+	static Model *twiceDividedPath;
+	static Model *archPath;
+	static Model *curvedPath;
+	static Model *speedupBlock;
+	static Model *slowdownBlock;
 };
