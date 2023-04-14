43d0449a62199c3049e35f211aad2d8914b0384e
Ratik Kapoor <ratikrk@gmail.com>
2023-04-14 14:20:38 -0600

    feat: Changed step physics height

--- DustRiders/DustRiders/Obstacle.cpp
+++ DustRiders/DustRiders/Obstacle.cpp
@@ -1,23 +1,23 @@
 #include "Obstacle.h"
 
 Obstacle::Obstacle(std::string n,
-									 Model *m,
-									 ShaderProgram *sp,
-									 glm::vec3 s,
-									 PhysicsProvider *pp,
-									 PxVec3 pos = {0.f, 0.f, 0.f},
-									 unsigned int mat = 0) : PhysicsEntity(n, m, sp, s, pp, pos, mat)
+	Model* m,
+	ShaderProgram* sp,
+	glm::vec3 s,
+	PhysicsProvider* pp,
+	PxVec3 pos = { 0.f, 0.f, 0.f },
+	unsigned int mat = 0) : PhysicsEntity(n, m, sp, s, pp, pos, mat)
 {
 	initObstacle(pos);
 }
 
 void Obstacle::initObstacle(PxVec3 pos)
 {
-	auto shape = this->gPhysics->createShape(PxBoxGeometry(0.5, 0.5, 0.5), *gMaterial);
+	auto shape = this->gPhysics->createShape(PxBoxGeometry(0.5, 0.2, 0.5), *gMaterial);
 	shape->setFlag(PxShapeFlag::eSIMULATION_SHAPE, true);
 
 	PxTransform localTm(pos);
-	PxRigidDynamic *body = gPhysics->createRigidDynamic(localTm);
+	PxRigidDynamic* body = gPhysics->createRigidDynamic(localTm);
 	body->setGlobalPose(localTm);
 	body->setRigidBodyFlag(PxRigidBodyFlag::eKINEMATIC, true);
 	body->attachShape(*shape);
